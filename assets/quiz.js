0054a295882952b172097c640fa97ba0d2be8814
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-05-14 16:37:53 -0700

    store and update answers in objects

--- assets/quiz.js
+++ assets/quiz.js
@@ -5,18 +5,14 @@ const answers = {};
 
 quiz_form.addEventListener("change", updateAnswers);
 function updateAnswers(event) {
-  //code to store answers
-
-
-
-  console.log("answers: ", answers);
+    //code to store answers
+    answers[event.target.name] = event.target.value;
 }
 
-
 finish_btn.addEventListener("click", function () {
-  if (answers.product_type && answers.bike_type && answers.color) {
-    alert("Here are your answers: " + JSON.stringify(answers));
-  } else {
-    alert("Please answer all the questions!");
-  }
+    if (answers.product_type && answers.bike_type && answers.color) {
+        alert("Here are your answers: " + JSON.stringify(answers));
+    } else {
+        alert("Please answer all the questions!");
+    }
 });
