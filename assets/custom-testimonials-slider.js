e1c2bba53009c45011af848e9d079738395a4792
zxcvanikabnm <zxcvanikabnm@gmail.com>
2025-05-31 23:52:06 -0700

    Testimonial blocks rendered dynamically and initialized slider

--- assets/custom-testimonials-slider.js
+++ assets/custom-testimonials-slider.js
@@ -2,8 +2,17 @@
 
 // config:
 // assume there is just one single slider on the page
-// navigation dots should be disabled
-// no autoplay
-// infinite loop
-// the slides or cells should be aligned to the left side on initialization.
-// pauseAutoPlayOnHover: false
+const slideshow_container = document.querySelector(".testimonial-slider");
+const slider = new Flickity(slideshow_container, {
+    // the slides or cells should be aligned to the left side on initialization.
+    cellAlign: "left",
+    // infinite loop
+    wrapAround: true,
+    // no autoplay
+    autoPlay: false,
+    // pauseAutoPlayOnHover: false
+    pauseAutoPlayOnHover: false,
+    // navigation dots should be disabled
+    pageDots: false,
+    fullscreen: true,
+});
